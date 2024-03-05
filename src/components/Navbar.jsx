10e394fb2755dfdfc1442837e0927e9c80f52c73
Ivan Miasoied <voka009@gmail.com>
2024-03-06 01:30:49 +0300

    start prj

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-white'>
+        <button>Shop</button>
+        <button>Sopping cart</button>
+    </div>
+  )
+}
+
+export default Navbar
